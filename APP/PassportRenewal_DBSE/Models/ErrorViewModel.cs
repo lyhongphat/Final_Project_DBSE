f42897010b3162fadd83e3346ac6106475eaf009
khowfsix <lyhongphat261202@gmail.com>
2024-11-27 20:21:29 +0700

    nothing

--- APP/PassportRenewal_DBSE/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PassportRenewal_DBSE.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
