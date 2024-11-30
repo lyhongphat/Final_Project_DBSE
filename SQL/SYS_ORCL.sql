c809f06bcc6e9a14db97831a2985dba89dec80a7
khowfsix <lyhongphat261202@gmail.com>
2024-11-30 20:57:47 +0700

    bruh

--- SQL/SYS_ORCL.sql
+++ SQL/SYS_ORCL.sql
@@ -1,6 +1,17 @@
-ALTER PLUGGABLE DATABASE AlL OPEN;
-ALTER SYSTEM SET audit_trail= DB,EXTENDED SCOPE=SPFILE;
-SHUTDOWN IMMEDIATE
-STARTUP
+-- Open all pluggable databases
+ALTER PLUGGABLE DATABASE ALL OPEN;
+
+-- Set the audit trail to DB and extended, effective after the next startup
+ALTER SYSTEM SET audit_trail = DB, EXTENDED SCOPE = SPFILE;
+
+-- Shut down the database immediately
+SHUTDOWN IMMEDIATE;
+
+-- Start up the database
+STARTUP;
+
+-- Remove all records from the audit trail table
 TRUNCATE TABLE aud$;
+
+-- Remove all records from the fine-grained auditing log table
 TRUNCATE TABLE fga_log$;
